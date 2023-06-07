53dec8278b38fca287c46d49b11a68759b1642eb
tiger1028 <gentleman.tiger1028@gmail.com>
2023-06-07 14:58:24 -0400

    feat: add history captured position

--- src/components/common/historyPanel/index.tsx
+++ src/components/common/historyPanel/index.tsx
@@ -2,6 +2,7 @@ import { GamePlayer } from 'consts';
 import React, { useContext } from 'react';
 import './style.css';
 import { BoardContext } from 'contexts';
+import { convertCheckerPositionToString } from 'utils/position';
 
 export const HistoryPanelComponent: React.FC = () => {
   const { board } = useContext(BoardContext);
@@ -10,18 +11,25 @@ export const HistoryPanelComponent: React.FC = () => {
     <ul className="history-panel__container">
       {board.movementHistory.map((movement, index) => (
         <li key={index} className="record__container">
-          {movement.player === GamePlayer.BLUE
-            ? 'Blue'
-            : movement.player === GamePlayer.RED
-            ? 'Red'
-            : ''}
-          &nbsp;
-          {`${movement.fromPosition.row} ${movement.fromPosition.col}`}
+          <div>
+            {movement.player === GamePlayer.BLUE
+              ? 'Blue'
+              : movement.player === GamePlayer.RED
+              ? 'Red'
+              : ''}
+            &nbsp; ({convertCheckerPositionToString(movement.fromPosition)})
+            &nbsp;&gt;&nbsp; (
+            {convertCheckerPositionToString(movement.toPosition)})
+          </div>
+          {!!movement.capturedChecker.length &&
+            movement.capturedChecker.map((capturedChecker, index) => (
+              <div key={index}>
+                Captured&nbsp;
+                {convertCheckerPositionToString(capturedChecker.position)}
+              </div>
+            ))}
         </li>
       ))}
-      {/* <li className="record__container">First</li>
-      <li className="record__container">Second</li>
-      <li className="record__container">Third</li> */}
     </ul>
   );
 };
